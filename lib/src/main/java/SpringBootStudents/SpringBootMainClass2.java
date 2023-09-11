4ad0324ad2ea91c532e96d2e63862e3fccbf5234
vykuntaraoE <vykuntarao.e@gmail.com>
2023-09-11 22:12:09 +0530

    6666

---
+++ lib/src/main/java/SpringBootStudents/SpringBootMainClass2.java
@@ -0,0 +1,17 @@
+package SpringBootStudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMainClass2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMainClass2.class, args);
+		
+		System.out.println("Spring boot main class output");
+		System.out.println(" for commit pupose 6666");
+		
+	}
+
+}
