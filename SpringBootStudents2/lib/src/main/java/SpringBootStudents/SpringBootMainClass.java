ad2aaff046d9f3380ded16acdc998da6c66e108b
vykuntaraoE <vykuntarao.e@gmail.com>
2023-09-11 20:30:37 +0530

    I have uploaded 2nd springboot students project

---
+++ SpringBootStudents2/lib/src/main/java/SpringBootStudents/SpringBootMainClass.java
@@ -0,0 +1,15 @@
+package SpringBootStudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMainClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMainClass.class, args);
+		
+		System.out.println("Spring boot main class output");
+	}
+
+}
